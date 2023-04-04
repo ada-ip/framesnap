1ede4b5fa34755412e601200b80e7936f38988c2
ada-i-puig <ada.i@puigadam.es>
2023-04-04 19:20:39 +0200

    Arregla el filtro y la ordenación de los posts que se pasan al index

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const Post = require("../models/Post");
+const mongoose = require("mongoose");
 
 const devolverIndex = async (req, res, next) => {
 	if (!req.session.idUsuario) {
@@ -8,12 +9,34 @@ const devolverIndex = async (req, res, next) => {
 		try {
 			const usuario = await User.findById(req.session.idUsuario).select("_id nombre fotoPerfil seguidos tls");
 
-			const autoresPosts = {
-				autor: [req.session.idUsuario]
-			};
-			usuario.seguidos.forEach((usuarioSeguido) => autoresPosts.autor.push(usuarioSeguido));
-			const posts = await Post.find(autoresPosts).populate({ path: "autor", select: "nombre fotoPerfil" });
-			console.log(posts);
+			if (!usuario) {
+				return res.status(404).end();
+			}
+
+			const filtro = {};
+			if (usuario.tls[0].config.filtro.autor.length > 0) {
+				filtro.autor = usuario.tls[0].config.filtro.autor;
+			}
+			if (usuario.tls[0].config.filtro.tags.length > 0) {
+				filtro.tags = usuario.tls[0].config.filtro.tags;
+			}
+			if (usuario.tls[0].config.filtro.fecha) {
+				filtro.fecha = usuario.tls[0].config.filtro.fecha;
+			}
+
+			let orden = "";
+			if (usuario.tls[0].config.orden > 0) {
+				for (let campo of usuario.tls[0].config.orden) {
+					orden += campo + " ";
+				}
+			}
+
+			const posts = await Post.find(filtro)
+				.populate({
+					path: "autor",
+					select: "nombre fotoPerfil"
+				})
+				.sort(orden);
 
 			res.render("index", { usuario: usuario, posts: posts });
 		} catch (error) {
