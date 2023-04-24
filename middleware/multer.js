a967456e65f7289b6947af262dc037712db296e4
ada-ip <ada.iglesias.pasamontes@gmail.com>
2023-04-24 21:21:42 +0200

    Modifica la compresión de las imágenes para reducir el tamaño de las mismas

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,15 +2,16 @@ const multer = require("multer");
 const { s3 } = require("../config/aws");
 const sharp = require("sharp");
 
-const MAX_TAMANYO = 300 * 1024;
-const TAMANYO_GRANDE = 4000 * 1024;
-const MAX_ANCHO = 1600;
+const TAMANYO_MEDIANO = 200 * 1024;
+const TAMANYO_GRANDE = 1000 * 1024;
+const TAMANYO_MUY_GRANDE = 1700 * 1024;
+const MAX_ANCHO = 1200;
 
 const customStorage = multer.memoryStorage();
 
 const cargarImagen = multer({
 	storage: customStorage,
-	limits: { fileSize: 8000000 }
+	limits: { fileSize: 8000000 },
 });
 
 const subirImagenAS3 = async (req, res, next) => {
@@ -18,14 +19,16 @@ const subirImagenAS3 = async (req, res, next) => {
 		const imagen = req.file;
 		let imagenBuffer = imagen.buffer;
 
-		if (imagen.size > TAMANYO_GRANDE) {
-			imagenBuffer = await sharp(imagen.buffer).rotate().resize({ width: 1600 }).jpeg({ quality: 55 }).toBuffer();
-		} else if (imagen.size > MAX_TAMANYO) {
+		if (imagen.size > TAMANYO_MUY_GRANDE) {
+			imagenBuffer = await sharp(imagen.buffer).rotate().resize({ width: 1200 }).jpeg({ quality: 50 }).toBuffer();
+		} else if (imagen.size > TAMANYO_GRANDE) {
+			imagenBuffer = await sharp(imagen.buffer).rotate().resize({ width: 1200 }).jpeg({ quality: 65 }).toBuffer();
+		} else if (imagen.size > TAMANYO_MEDIANO) {
 			const metadata = await sharp(imagen.buffer).metadata();
 			const ancho = metadata.width;
 
 			if (ancho > MAX_ANCHO) {
-				imagenBuffer = await sharp(imagen.buffer).rotate().resize({ width: 1600 }).jpeg({ quality: 80 }).toBuffer();
+				imagenBuffer = await sharp(imagen.buffer).rotate().resize({ width: 1200 }).jpeg({ quality: 70 }).toBuffer();
 			} else {
 				imagenBuffer = await sharp(imagen.buffer).rotate().jpeg({ quality: 80 }).toBuffer();
 			}
@@ -35,7 +38,7 @@ const subirImagenAS3 = async (req, res, next) => {
 			Bucket: process.env.AWS_BUCKET_NAME,
 			Key: `images/${Date.now().toString()}-${imagen.originalname}`,
 			Body: imagenBuffer,
-			ContentType: imagen.mimetype
+			ContentType: imagen.mimetype,
 		};
 
 		s3.upload(params, (error, data) => {
